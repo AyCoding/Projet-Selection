94ae29d59ec49a06af5e7f96a59788a08001d3a6
Ertinox-08 <aymericdelange80@gmail.com>
2022-11-30 12:03:00 +0100

    Update

--- controller/accountManager.php
+++ controller/accountManager.php
@@ -1,1 +1,14 @@
 <?php
+include "db/database.php";
+// Connexion à la BDD
+$db = new PDO("mysql:host=" . $dbServename . ";dbname=" . $dbName, $dbUsername, $dbPassword);
+
+// On récupère dans la table "user" tout les utilisateurs et les mots de passe
+$sql = "SELECT * FROM `users`";
+
+// On prépare la lecture de BDD
+$result = $db->prepare($sql);
+$result->execute();
+
+// Récuperer la ligne
+$data_admin = $result->fetch();
